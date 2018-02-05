f4bb66ebe1f0d3fe1cc601abd3a84d66b2117d78
TonyFord <mail2@strategy-investor.de>
2018-02-05 22:57:20 +0100

    updates

--- setup/botc_growth.js
+++ setup/botc_growth.js
@@ -1,28 +1,33 @@
 function ChartOnLoad(){
 
-  var J=json_load("http://api.fairplayground.info/rawdata/FCLN.geo.json","json");
+  var J=json_load("http://api.fairplayground.info/rawdata/botc_growth.csv","csv");
 
+  /*
   var K=[];
 
-  var J=J.features[0];
+  var J=J.split("\n");
 
   J.forEach(
     function( v,i ){
-      K.push( [ v.properties.establishing_assembly, i ] );
+      K.push( [ v[0], i ] );
     }
   );
 
+  */
+
   var L="";
 
-  K.sort().reverse();
+  //K.sort().reverse();
 
+  /*
   K.forEach(
     function( v,i ){
       L+= J[ v[1] ].properties.name + " " + J[ v[1] ].properties.establishing_assembly.slice(0,7) + "<br>";
     }
   );
+  */
 
-
+  /*
   $($(".row:last-child .col")[0]).html( L );
-
+  */
 }
